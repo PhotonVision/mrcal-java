54cb41bc1c4b6548ff6fcc290e4e2f659f2f1182
Matt M <matthew.morley.ca@gmail.com>
2026-01-24 19:50:28 -0800

    waaah

--- cal-study.sh
+++ cal-study.sh
@@ -6,13 +6,14 @@ mrgingham --jobs 8 --gridn 10 $PICTURES > corners.vnl
 #   vnl-filter -p x,y | \
 #   feedgnuplot --domain --square --set 'xrange [0:1280] noextend' --set 'yrange [720:0] noextend' &
 
-# mrcal-calibrate-cameras         \
-#   --corners-cache corners.vnl   \
-#   --lensmodel LENSMODEL_OPENCV8 \
-#   --focal 1000                  \
-#   --object-spacing 0.0254       \
-#   --object-width-n 10           \
-#   "/home/matth/mrcal-java/test_pictures/1280preduced/*"
+cd mrcal
+./mrcal-calibrate-cameras         \
+  --corners-cache ../corners.vnl   \
+  --lensmodel LENSMODEL_OPENCV8 \
+  --focal 1000                  \
+  --object-spacing 0.0254       \
+  --object-width-n 10           \
+  "/home/matth/mrcal-java/test_pictures/1280preduced/*"
 
 # mrcal-show-geometry      \
 #   ./camera-0.cameramodel \
@@ -32,4 +33,4 @@ mrgingham --jobs 8 --gridn 10 $PICTURES > corners.vnl
 #   --magnitudes                         \
 #   --set 'cbrange [0:1.5]' ./camera-0.cameramodel &
 
-mrcal-show-projection-uncertainty ./camera-0.cameramodel --cbmax 10 --unset key &
+./mrcal-show-projection-uncertainty ./camera-0.cameramodel --cbmax 10 --unset key &
