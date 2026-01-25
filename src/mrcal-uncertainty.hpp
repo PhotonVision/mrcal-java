943efd0887a65ba3b8e9bafa43c16638704dbb8e
Matt M <matthew.morley.ca@gmail.com>
2026-01-24 20:17:01 -0800

    awooga

--- src/mrcal-uncertainty.hpp
+++ src/mrcal-uncertainty.hpp
@@ -9,6 +9,18 @@
 using namespace cv;
 
 template <>
+struct fmt::formatter<mrcal_point2_t> {
+    constexpr auto parse(format_parse_context& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const mrcal_point2_t& p, FormatContext& ctx) const {
+        return fmt::format_to(ctx.out(), "({}, {})", p.x, p.y);
+    }
+};
+
+template <>
 struct fmt::formatter<mrcal_point3_t> {
     constexpr auto parse(format_parse_context& ctx) {
         return ctx.begin();
@@ -20,22 +32,50 @@ struct fmt::formatter<mrcal_point3_t> {
     }
 };
 
-std::vector<mrcal_point3_t> sample_imager(Size numSamples, Size imagerSize) {
-    // using std::ranges, sample the imager with numSamples points per axis
-    std::vector<mrcal_point3_t> samples;
-    for (int i = 0; i < numSamples.width; i++) {
-        for (int j = 0; j < numSamples.height; j++) {
-            double x = (i + 0.5) * imagerSize.width / numSamples.width;
-            double y = (j + 0.5) * imagerSize.height / numSamples.height;
-            samples.push_back({.x = x, .y = y, .z = 1.0});
+std::vector<mrcal_point2_t> sample_imager(Size numSamples, Size imagerSize) {
+    std::vector<mrcal_point2_t> samples;
+    samples.reserve(numSamples.width * numSamples.height);
+    
+    for (int j = 0; j < numSamples.height; j++) {
+        for (int i = 0; i < numSamples.width; i++) {
+            double x, y;
+            
+            // linspace formula: start + (stop - start) * i / (n - 1)
+            if (numSamples.width == 1) {
+                x = (imagerSize.width - 1) / 2.0;
+            } else {
+                x = (imagerSize.width - 1) * i / (numSamples.width - 1.0);
+            }
+            
+            if (numSamples.height == 1) {
+                y = (imagerSize.height - 1) / 2.0;
+            } else {
+                y = (imagerSize.height - 1) * j / (numSamples.height - 1.0);
+            }
+            
+            samples.push_back({.x = x, .y = y});
         }
     }
     return samples;
 }
 
 void compute_uncertainty() {
+    mrcal_lensmodel_t lensmodel;
+    lensmodel.type = MRCAL_LENSMODEL_OPENCV8;
+    std::vector<double> intrinsics {
+        1088.363893, 1095.830102, 608.3108394, 355.4729472, 0.2922172061, -1.429731317, -0.001481463652, 0.004184715836, 2.570603373, 0.0639153706, 0.07765775163, -0.09689803946,};
+
     // hard code some stuff
     auto q = sample_imager({60, 40}, {1280, 720});
-    fmt::print("Samples: {}\n", q);
+    fmt::print("Samples: {}, len={}\n", q, q.size());
+
+    // and unproject
+    std::vector<mrcal_point3_t> out;
+    out.reserve(q.size());
+    mrcal_unproject(
+        out.data(), q.data(), q.size(),
+        &lensmodel, intrinsics.data()
+    );
+    fmt::print("Unprojected: {}, len={}\n", out, out.size());
 }
     
