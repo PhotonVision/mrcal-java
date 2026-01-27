9241a780349193641dd127b698ed9d15679e2c3b
Matt M <matthew.morley.ca@gmail.com>
2026-01-26 20:33:14 -0800

    Let's just plot one point

--- src/mrcal-uncertainty.hpp
+++ src/mrcal-uncertainty.hpp
@@ -42,13 +42,13 @@ std::vector<mrcal_point2_t> sample_imager(Size numSamples, Size imagerSize) {
             
             // linspace formula: start + (stop - start) * i / (n - 1)
             if (numSamples.width == 1) {
-                x = (imagerSize.width - 1) / 2.0;
+                x=0;
             } else {
                 x = (imagerSize.width - 1) * i / (numSamples.width - 1.0);
             }
             
             if (numSamples.height == 1) {
-                y = (imagerSize.height - 1) / 2.0;
+                y=0;
             } else {
                 y = (imagerSize.height - 1) * j / (numSamples.height - 1.0);
             }
@@ -59,30 +59,72 @@ std::vector<mrcal_point2_t> sample_imager(Size numSamples, Size imagerSize) {
     return samples;
 }
 
-void projection_uncertainty(pcam) {
+// The derivative of q (pixel space location/s) wrt b (state vector)
+// at some point this should be a matrix
+std::vector<std::vector<double>> _dq_db_projection_uncertainty(
+    std::vector<mrcal_point3_t> pcam,
+    mrcal_lensmodel_t lensmodel,
+    std::vector<double> intrinsics
+) {
+    // project with gradients
+    std::vector<mrcal_point2_t> q(pcam.size());
+    std::vector<mrcal_point3_t> dq_dp(pcam.size()*2);
+    std::vector<double> dq_dintrinsics(2*pcam.size()*intrinsics.size()); 
+    bool ret = mrcal_project(
+        // out
+        q.data(),
+        dq_dp.data(),
+        dq_dintrinsics.data(),
+        // in
+        pcam.data(), pcam.size(),
+        &lensmodel, intrinsics.data()
+    );
 
+    if (!ret) {
+        throw std::runtime_error("mrcal_project_with_gradients failed");
+    }
+
+    // prepare dq_db. Mrcal does this as a 40x60x2xNstate tensor, but we
+    // flatten to a 2D array of shape (2*40*60, Nstate)
+
+
+    return {};
+}
+
+bool _propagate_calibration_uncertainty() {
+    return true;
+}
+
+bool projection_uncertainty(std::vector<mrcal_point3_t> pcam, 
+    /* method always mean_pcam, at_infinity always true */
+    /* what always worstdirection-stdev */
+    mrcal_lensmodel_t lensmodel,
+    std::vector<double> intrinsics
+    ) {
+    auto dq_db {_dq_db_projection_uncertainty(pcam, lensmodel, intrinsics)};
+    return _propagate_calibration_uncertainty();
 }
 
 void compute_uncertainty() {
     mrcal_lensmodel_t lensmodel;
     lensmodel.type = MRCAL_LENSMODEL_OPENCV8;
     std::vector<double> intrinsics {
-        1088.363893, 1095.830102, 608.3108394, 355.4729472, 0.2922172061, -1.429731317, -0.001481463652, 0.004184715836, 2.570603373, 0.0639153706, 0.07765775163, -0.09689803946,};
+        1129.780043, 1134.155189, 602.9912814, 371.6042914, 0.4825503559, -1.184116885, 0.006642882849, -0.003419705443, 2.387477579, 0.3025651431, 0.1612900202, -0.06008374044};
 
     // hard code some stuff
-    auto q = sample_imager({60, 40}, {1280, 720});
+    auto q = sample_imager({1, 1}, {1280, 720});
     fmt::print("Samples: {}, len={}\n", q[q.size()-1], q.size());
 
     // and unproject
-    std::vector<mrcal_point3_t> out;
-    out.resize(q.size());
+    std::vector<mrcal_point3_t> pcam;
+    pcam.resize(q.size());
     mrcal_unproject(
-        out.data(), q.data(), q.size(),
+        pcam.data(), q.data(), q.size(),
         &lensmodel, intrinsics.data()
     );
 
     // Always normalize, setting rows with any non-finite elements to zero
-    for (auto& p : out) {
+    for (auto& p : pcam) {
         double nrm = std::sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
         if (std::isfinite(nrm) && nrm > 0) {
             p.x /= nrm;
@@ -96,6 +138,12 @@ void compute_uncertainty() {
         }
     }
 
-    fmt::print("Unprojected: {}, len={}\n", out[out.size()-1], out.size());
+    fmt::print("Unprojected: {}, len={}\n", pcam[pcam.size()-1], pcam.size());
+
+    projection_uncertainty(
+        pcam,
+        lensmodel,
+        intrinsics
+    );
 }
     
