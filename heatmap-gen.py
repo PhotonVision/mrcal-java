2eb67c57ef3263ac72d0be168c8bd145c3252c36
Matt M <matthew.morley.ca@gmail.com>
2026-01-31 00:10:25 -0800

    whee

--- heatmap-gen.py
+++ heatmap-gen.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,11 +12,13 @@ z = data[:, 2]
 plt.figure()
 plt.title("Projection Uncertainty (in pixels), looking out to infinity")
 
+print(f"Mean={np.mean(z)}")
+
 # Create contour plot with 1px increments
-levels = np.arange(0, 11, 1)  # 0, 1, 2, ..., 10
+levels = np.arange(0, math.ceil(np.max(z)), 0.1)  # 0, 1, 2, ..., 10
 contour = plt.tricontour(x, y, z, levels=levels, colors="black", linewidths=0.5)
 contourf = plt.tricontourf(x, y, z, levels=levels, cmap="viridis")
-plt.clabel(contour, inline=True, fontsize=8, fmt="%1.0f px")
+plt.clabel(contour, inline=True, fontsize=8, fmt="%0.1f px")
 plt.colorbar(contourf, label="Uncertainty (px)")
 
 plt.xlabel("x")
